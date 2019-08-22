deb2d581381d77b0649543f4316e46b981d65cec
Tim Bakkum <timbakkum@gmail.com>
2019-08-22 13:49:56 +0200

    fix: remove comment

--- packages/web/nuxt.config.js
+++ packages/web/nuxt.config.js
@@ -1,7 +1,7 @@
-//if (process.env.NODE_ENV !== 'production') {
-require('dotenv').config()
-//console.log
-//}
+// comment out this conditional to run nuxt generate locally
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 export default {
   mode: 'universal',
